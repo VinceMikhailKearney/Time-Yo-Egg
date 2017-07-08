cab5d81018689efe7112453f9b1030100941b751
Vince <vincent.kearney@saltdna.com>
2017-07-08 20:39:11 +0100

    Initial Commit

---
+++ Time Yo Egg/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Time Yo Egg
+//
+//  Created by Vince Kearney on 08/07/2017.
+//  Copyright © 2017 vince. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
