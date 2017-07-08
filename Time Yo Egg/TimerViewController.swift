5a704be4c841ce920a2efafbcd0fbbe34c9f496a
Vince <vincent.kearney@saltdna.com>
2017-07-08 21:11:14 +0100

    Getting set up with my first macOS app. Added assets etc and a bit on the storyboard

---
+++ Time Yo Egg/TimerViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Time Yo Egg
+//
+//  Created by Vince Kearney on 08/07/2017.
+//  Copyright © 2017 vince. All rights reserved.
+//
+
+import Cocoa
+
+class TimerViewController: NSViewController
+{
+    // MARK: Properties
+    @IBOutlet weak var eggTimeTextLabel : NSTextField!
+    @IBOutlet weak var eggTimerImageView : NSImageView!
+    @IBOutlet weak var startButton : NSButton!
+    @IBOutlet weak var stopButton : NSButton!
+    @IBOutlet weak var restartButton : NSButton!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    /// TODO - Need to figure out what this is exactly
+    override var representedObject: Any?
+    {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+    
+    // MARK: Actions
+    @IBAction func clickStart(_ sender : Any) {
+        
+    }
+    
+    @IBAction func clickStop(_ sender : Any) {
+        
+    }
+    
+    @IBAction func clickRestart(_ sender : Any) {
+        
+    }
+}
+
