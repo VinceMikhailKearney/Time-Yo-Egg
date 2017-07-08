2eb23c1e9414d07317827afaaddf7610e8d31051
Vince <vincent.kearney@saltdna.com>
2017-07-08 21:48:13 +0100

    Adding preferences window and setting up the VC

---
+++ Time Yo Egg/PreferencesViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PreferencesViewController.swift
+//  Time Yo Egg
+//
+//  Created by Vince Kearney on 08/07/2017.
+//  Copyright © 2017 vince. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesViewController: NSViewController
+{
+    // MARK: Properties
+    @IBOutlet weak var popUp: NSPopUpButton!
+    @IBOutlet weak var slider: NSSlider!
+    @IBOutlet weak var eggTimeTextField: NSTextField!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Actions
+    @IBAction func popUpValueChanged(_ sender: Any) {
+        
+    }
+    
+    @IBAction func okButtonClicked(_ sender: Any) {
+        
+    }
+    
+    @IBAction func cancelButtonClicked(_ sender: Any) {
+        
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: Any) {
+        
+    }
+}
